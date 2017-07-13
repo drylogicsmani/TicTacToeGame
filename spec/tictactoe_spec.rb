71698c5afd403b382092369de42d8677ced9800e
drylogicsmani <manikandan@drylogics.com>
2017-07-13 18:42:20 +0530

    Initialize configuration of tictactoe

---
+++ spec/tictactoe_spec.rb
@@ -0,0 +1,37 @@
+describe "TicTacToeGame" do
+
+  it "should allow only two tokens" do
+  end
+
+  it "should both tokens are not same" do
+  end
+
+  it "should place tokens alternatively" do
+  end
+
+  it "should not place tokens for different places" do
+  end
+
+  context "Wining Condition" do
+
+    it 'should same token are placed in horizontally' do
+    end
+
+    it 'should same token are placed in vertically' do
+    end
+
+    it 'should same token are placed in diagonally' do
+    end
+
+  end
+
+  context "Draw Condition" do
+
+    it 'should token placed in all the cells' do
+    end
+
+    it 'should not satisfied wining condition' do
+    end
+    
+  end
+end
